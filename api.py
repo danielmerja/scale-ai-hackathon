5c3d24945a2e880f56ad26ca245c3a6987209d59
Benny Jiang <bennyjiangxinhao@gmail.com>
2023-07-15 12:31:19 -0700

    add chat functions

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import glob
 from flask import Flask, request, jsonify
 import logging
+from llms.openai import openaif
+import os
 
 
 app = Flask(__name__) 
@@ -19,6 +21,14 @@ def chat():
     return {"reply": "Message Received!" + message}
 
 
+# message / reply format
+@app.route("/analyze", methods=["POST", "GET"])
+def analyze():
+    tasks = collect_tasks()
+    answer = ask_ai(tasks)
+    return {"reply": "Message Received!" + message}
+
+
 @app.route("/sendpage", methods=["POST", "GET"])
 def sendpage():
     doc = request.json["message"]
@@ -27,3 +37,15 @@ def sendpage():
     body = doc['body']
 
     return {"reply": "Analysis Complete!"}
+
+
+def collect_tasks():
+    return 'string of tasks'
+
+def ask_ai(tasks):
+    open_ai = openaif(api_key=os.environ['OPENAI_API_KEY'])
+    # Analyze Prompt
+    prompt = f"Here are the tasks we have created: {tasks}, and please give us suggestion on a new task.\
+        Is it contradictory or dupliate to any previous task we have? If so, please give us the title \
+        of the task and the reason why it is contradictory or duplicate."
+    return open_ai.user_request(prompt)
